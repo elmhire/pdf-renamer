59b7cc161451dc5c1f72d7be81da081fd2157f29
Peter Wirtz <pwirtz13@gmail.com>
2021-04-10 00:41:21 -0500

    first commit

---
+++ go.mod
@@ -0,0 +1,14 @@
+module LED-CLI
+
+go 1.16
+
+require (
+	github.com/DusanKasan/parsemail v1.2.0
+	github.com/k0kubun/colorstring v0.0.0-20150214042306-9440f1994b88 // indirect
+	github.com/k0kubun/pp v3.0.1+incompatible // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/saintfish/chardet v0.0.0-20120816061221-3af4cd4741ca // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	github.com/timakin/gonvert v0.0.0-20170112000238-5dce59dbd0d8
+	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4
+)
---
+++ main.go
@@ -0,0 +1,289 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+
+	"github.com/DusanKasan/parsemail"
+	"github.com/timakin/gonvert"
+	"golang.org/x/net/html"
+)
+
+func check(err error) {
+	if err != nil {
+		fmt.Printf("Failed in check(): %s\n", err)
+		pause()
+		os.Exit(1)
+	}
+}
+
+func main() {
+	// Get email file name input from user
+	filename := getFileName()
+
+	// Get data from file
+	fmt.Printf("Opening %s...\n\n", filename)
+	data := getRawFileDataAsStr(filename)
+	content := convertToUTF8(data)
+
+	// Extract links from contents
+	links, err := extractLinks(content)
+	if err != nil {
+		fmt.Println("No files found to download.")
+		normalExit()
+	}
+
+	// Download links
+	fmt.Printf("%d file's to download...\n", len(links))
+	complete := downloadLinks(links)
+	fmt.Printf("%d of %d files downloaded completely.\n\n", complete, len(links))
+
+	normalExit()
+} // End main
+
+func getCwdFileList() []string {
+	var (
+		listing []string
+		fTypes  = []string{"htm", "html", "eml"}
+	)
+
+	list, err := os.ReadDir(".")
+	check(err)
+
+	for _, f := range list {
+		if filepath.Base(os.Args[0]) != f.Name() && !f.IsDir() && existsIn(fTypes, f.Name()) {
+			listing = append(listing, f.Name())
+		}
+	}
+	return listing
+}
+
+func existsIn(list []string, a string) bool {
+	for _, b := range list {
+		if strings.Contains(a, strings.ToLower(b)) {
+			return true
+		}
+	}
+	return false
+}
+
+func getFileName() string {
+	files := getCwdFileList()
+	var choice string
+	for {
+		fmt.Println("Please select the email to open.")
+		fmt.Println()
+		for i, file := range files {
+			fmt.Printf("(%d) %s\n", i+1, file)
+		}
+
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Printf("\n (q to exit)-> ")
+		input, _ := reader.ReadString('\n')
+
+		input = strings.TrimSpace(input)
+		if strings.ToLower(input) == "q" {
+			fmt.Println("Exiting...")
+			pause()
+			os.Exit(0)
+		}
+		ans, err := strconv.ParseUint(input, 10, 16)
+		if err != nil || int(ans) >= len(files)+1 || int(ans) < 1 {
+			fmt.Println("\nInvalid entry, please try again.")
+			fmt.Println()
+			continue
+		} else {
+			fmt.Printf("\nYou selected: %v \n\n", ans)
+			choice = files[ans-1]
+			break
+		}
+	}
+	return choice
+}
+
+func getRawFileDataAsStr(filename string) string {
+	var data string
+
+	if strings.Contains(filename, "eml") {
+		data = getEMLContent(filename)
+	} else {
+		data = getHTMLContent(filename)
+	}
+	return string(data)
+}
+
+func getEMLContent(filename string) string {
+	rd := getBytesReaderFromFile(filename)
+
+	email, err := parsemail.Parse(rd)
+	if err == io.EOF {
+		fmt.Println("Empty file:", filename, "\nExiting.")
+		pause()
+		os.Exit(0)
+	} else if err != nil {
+		fmt.Println("Error reading file:", filename)
+		fmt.Println("Either it is corrupt or it is not an 'eml' file.")
+		pause()
+		os.Exit(1)
+	}
+	html := email.HTMLBody
+	var retVal string
+	decoded, err := base64.StdEncoding.DecodeString(html)
+	if err != nil {
+		fmt.Println("Data not base64 encoded.")
+		fmt.Println()
+		retVal = html
+	} else {
+		fmt.Println("Data is base64 encoded.")
+		fmt.Println()
+		retVal = string(decoded)
+	}
+	return retVal
+}
+
+func getHTMLContent(filename string) string {
+	temp, err := os.ReadFile(filename)
+	check(err)
+	return string(temp)
+}
+
+func convertToUTF8(data string) string {
+	content, err := gonvert.New(data, gonvert.UTF8).Convert()
+	if err != nil {
+		fmt.Println("Failed to Convert: ", err)
+		pause()
+		os.Exit(1)
+	}
+	return content
+}
+
+func getBytesReaderFromFile(filename string) *bytes.Reader {
+	dat, err := os.ReadFile(filename)
+	if err != nil {
+		log.Printf("Failed opening %s: %s", filename, err)
+		pause()
+		os.Exit(1)
+	}
+	return bytes.NewReader(dat)
+}
+
+// Link ...
+type Link struct {
+	url  string
+	text string
+}
+
+func extractLinks(htmlText string) ([]Link, error) {
+	var links []Link
+
+	node, err := html.Parse(strings.NewReader(htmlText))
+	check(err)
+
+	// Algorithm taken from https://pkg.go.dev/golang.org/x/net/html#pkg-functions
+	// Takes an html.Node and walks the tree recursivly
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, a := range n.Attr {
+				if a.Key == "href" &&
+					!strings.Contains(a.Val, "mailto") &&
+					!strings.Contains(n.FirstChild.Data, "here") {
+					link := Link{
+						url:  a.Val,
+						text: n.FirstChild.Data,
+					}
+					links = append(links, link)
+					break
+				}
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+	f(node)
+	if links == nil {
+		return nil, errors.New("No links found.")
+	}
+	return links, nil
+}
+
+func downloadLinks(links []Link) int {
+	complete := 0
+	for _, link := range links {
+		fmt.Printf("Downloading %s...", link.text)
+		err := downloadFile(link, true)
+		if err != nil {
+			fmt.Printf("\terror, incomplete!\n")
+			continue
+		}
+		fmt.Printf("\tcomplete.\n")
+		complete++
+	}
+	return complete
+}
+
+func downloadFile(link Link, useLinkName bool, pathOptional ...string) error {
+	var (
+		path     string
+		filename string
+	)
+
+	if len(pathOptional) > 0 {
+		path = pathOptional[0]
+	} else {
+		path = "."
+	}
+
+	response, err := http.Get(link.url)
+	check(err)
+
+	defer response.Body.Close()
+
+	// Decide whether to use link.text as filename
+	// or the name that the server gives us
+	if useLinkName {
+		filename = fmt.Sprintf("%s/%s", path, link.text)
+	} else {
+		filename = filepath.Base(response.Request.URL.EscapedPath())
+		temp, err := url.PathUnescape(filename)
+		if err == nil {
+			filename = temp
+		}
+	}
+
+	// Create the file
+	out, err := os.Create(fmt.Sprintf("%s/%s", path, filename))
+	if err != nil {
+		return err
+	}
+
+	defer out.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, response.Body)
+	return err
+}
+
+func normalExit() {
+	fmt.Print("\nPress 'Enter' to continue...")
+	pause()
+}
+
+func pause() {
+	_, err := bufio.NewReader(os.Stdin).ReadBytes('\n')
+	if err != nil {
+		log.Fatal("Something went wrong: ", err)
+	}
+}
